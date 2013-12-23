4e4a2cc2c17f8e901a57b81eda1abd19eb49674d
yingf <devnull@localhost>
2013-12-23 14:24:16 +0800

    node-oracle测试

---
+++ node-oracle/select.js
@@ -0,0 +1,26 @@
+var oracle = require("oracle");
+
+var connectData = {"hostname": "localhost", "database": "xe" ,
+
+                   "user": "system", "password": "yinguangfei"};
+
+
+oracle.connect(connectData, function(err, connection) {
+
+  if ( err ) {
+    console.log(err);
+  } else {
+
+    connection.execute("SELECT * FROM NODE", [], function(err, results) {
+
+      if ( err ) {
+
+        console.log(err);
+
+      } else {
+        console.log(results);
+      }
+      connection.close();
+    });
+  }
+});
